b4cc0512132ebcfaa507832ee7765320b35079b5
rajeshkhatwa <61071234+rajeshkhatwa@users.noreply.github.com>
2024-01-31 21:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from langchain.prompts import (
 )
 
 print("Processing the Files")
-file_path = "/Users/I311450/Library/CloudStorage/OneDrive-SAPSE/Desktop/Tasks/api_key.txt"
+file_path = "/Tasks/api_key.txt"
 file_data = open(file_path, "r")
 api_key = file_data.readline()
 
